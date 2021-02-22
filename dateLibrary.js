eec04f88de172caafb1d7bdbf9b0d272831599ab
Dan <danazad@gmail.com>
2021-02-22 13:27:08 -0600

    Methods dayDiff monthDiff

--- dateLibrary.js
+++ dateLibrary.js
@@ -2,12 +2,12 @@ class WizLib {
     constructor(date = new Date()) { //Default value in constructor
         this.date = date;
         this.dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+        this.dayToday = this.dayOfWeek[this.date.getDay()];
         this.monthOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "Novermber", "December"];
         this.monthToday = this.monthOfYear[this.date.getMonth()];
-        this.dayToday = this.dayOfWeek[this.date.getDay()];
     }
 
-    time() {
+    timeArray() {
         let year = this.date.getFullYear();
         let month = this.monthOfYear[this.date.getMonth()];
         let day = this.dayOfWeek[this.date.getDay()];
@@ -144,11 +144,11 @@ class WizLib {
 
         d.setDate(d.getDate() - subtractDays);
 
-        return `${subtractDays} days before today ${d.toLocaleString()}`;
+        return `${subtractDays} day from now was ${d.toLocaleString()}`;
     }
 
     futureDay(addDays) {
-        let d = new Date();
+        let d = this.date;
 
         d.setDate(d.getDate() + addDays);
 
@@ -156,7 +156,7 @@ class WizLib {
     } 
 
     pastHour(subtractHour) {
-        let d = new Date();
+        let d = this.date;
 
         d.setHours(d.getHours() - subtractHour);
 
@@ -164,12 +164,98 @@ class WizLib {
     }
 
     futureHour(addHour) {
-        let d = new Date();
+        let d = this.date;
 
         d.setHours(d.getHours() + addHour);
 
-        return `${addHour} hours from now was ${d.toLocaleString()}`;
+        return `${addHour} hours from now is ${d.toLocaleString()}`;
     }
+
+    dayDifferenceYMD(date1, date2) { //Calculates difference between dates in days
+        const millisecondsPerDay = 1000 * 60 * 60 * 24;
+
+        // a and b are javascript Date objects
+        function dateDiffInDays(date1, date2) {
+        // Discard the time and time-zone information.
+        const utc1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate())   ;
+        const utc2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate())   ;
+
+        return Math.floor(Math.abs((utc2 - utc1) / millisecondsPerDay));
+        }
+
+        return dateDiffInDays(date1, date2); //diff date1 and date2
+    }
+
+    milisecondDifference(date1, date2) {
+        let millisecondDifference = date1 - date2;
+
+        let secondDifference = Math.floor(millisecondDifference / 1000);
+
+        return millisecondDifference;
+    }
+
+    secondDifference(date1, date2) {
+        let millisecondDifference = date1 - date2;
+
+        let secondDifference = Math.floor(millisecondDifference / 1000);
+
+        return secondDifference;
+    }
+
+    minuteDifference(date1, date2) {
+        let millisecondDifference = date1 - date2;
+        let secondDifference = Math.floor(millisecondDifference / 1000);
+
+        let minuteDifference = Math.floor(secondDifference / 60);
+
+        return minuteDifference;
+    }
+
+    hourDifference(date1, date2) {
+        let millisecondDifference = date1 - date2;
+        let secondDifference = Math.floor(millisecondDifference / 1000);
+        let minuteDifference = Math.floor(secondDifference / 60);
+        let hourDifference = Math.floor(minuteDifference / 60)
+
+        return hourDifference;
+    }
+
+    dayDifference(date1, date2) {
+        let millisecondDifference = date1 - date2;
+        let secondDifference = Math.floor(millisecondDifference / 1000);
+        let minuteDifference = Math.floor(secondDifference / 60);
+        let hourDifference = Math.floor(minuteDifference / 60)
+
+        let dayDifference = Math.floor(hourDifference / 24)
+
+        return dayDifference;
+    }
+
+    weekDifference(date1, date2) {
+        let millisecondDifference = date1 - date2;
+        let secondDifference = Math.floor(millisecondDifference / 1000);
+        let minuteDifference = Math.floor(secondDifference / 60);
+        let hourDifference = Math.floor(minuteDifference / 60)
+        let dayDifference = Math.floor(hourDifference / 24)
+
+        let weekDifference = Math.floor(dayDifference / 7)
+
+        return weekDifference;
+    }
+
+    monthDifference(date1, date2) {
+        let years = this.yearDifference(date1, date2);
+
+        let months = (years * 12) + (date2.getMonth() - date1.getMonth());
+
+        return months;
+    }
+
+    yearDifference(date1, date2) {
+        let yearDifference = date1.getFullYear() - date2.getFullYear();
+
+        return yearDifference;
+    }    
 }
 
 /*
